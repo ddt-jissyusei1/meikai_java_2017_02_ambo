dd6adf00a65652684af8a1032b8056067eddde21
ambo <ddt.jissyusei1@gmail.com>
2017-02-06 21:37:17 +0900

    演習１-１ セミコロンの欠如を確認

---
+++ Test1-1/src/Test1_1.java
@@ -0,0 +1,10 @@
+
+public class Test1_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+	System.out.println("初めてのJavaプログラム。");
+	System.out.println("画面に出力しています。");
+	}
+
+}
